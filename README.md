63886fa27cbf95df321c8a0a768fc62d4f5a80f4
Abhisman Sarkar <abhisman_sarkar@lineaje.com>
2023-11-20 16:09:29 +0530

    Added file main.py and updated README.md
    Signed-off-by: Abhisman Sarkar <abhisman_sarkar@lineaje.com>

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# page-llm-parser
+# Web Scraping and HTML Modification Script
+
+## Overview
+
+This Python script performs web scraping on a specified GitHub profile page and modifies the HTML content by removing, replacing, and keeping specific HTML tags and their attributes.
+
+## Language and Libraries
+
+1. **Python** was chosen as it is a simple language with support for web scraping and also has useful libraries for parsing a html document
+
+2. **BeautifulSoup:** was chosen for it providing a simple interface to parse the html document
+3. **Requests:** was used for making HTTP requests to get the page content
+4. **re** was used for data manipulation to change tag instances through regex
+
+## Strategy
+
+1. **Table data:** The table data present in the page was modified to keep count of the unique "aria-describedby" values alongside removing the thead tag in the table.
+2. **Tag Removal:** Identified and removed specific HTML tags using `decompose()` method based on the provided `tags_to_remove` list.
+
+3. **Tag Replacement:** Replaced selected tags with new tags according to the mappings specified in the `tags_to_replace` dictionary
+4. **Attribute Preservation:** Retained specific attributes for specified tags from the HTML document as specified in the `tags_to_store` dictionary.
+5. **Regex:** Used regex to replace all ending tags in the document with "</>" to signify a shorter closing tag
+
+## Usage
+
+To utilize this script:
+
+1. Clone the repository.
+2. Run `python3 main.py > out.html`
+3. View the modified page contents in `out.html`
+
+## Dependencies
+
+- **Python 3:** [Link to Python](https://www.python.org/)
+- **BeautifulSoup:** [Link to BeautifulSoup](https://www.crummy.com/software/BeautifulSoup/)
+- **Requests:** [Link to Requests](https://docs.python-requests.org/)
---
+++ main.py
@@ -0,0 +1,99 @@
+import requests
+from bs4 import BeautifulSoup
+from collections import Counter
+import re
+
+url = 'https://github.com/kaavee315'
+
+r = requests.get(url)
+
+soup = BeautifulSoup(r.text, 'html.parser')
+
+# Dictionary storing tags and their associated attributes to keep
+tags_to_store = {
+    "table": ["class"],
+    "input": ["class"],
+    "clipboard-copy": [],
+    "turbo-frame": ["id"],
+    "span": ["data-error-text", "data-success-text"],
+    "div": ["class", "role"],
+    "query-builder": ["class", "id"],
+    "style": [],
+    "label": ["id", "style"],
+    "svg": ["class"],
+    "meta": ["name"],
+    "tool-tip": ["data-type", "for"],
+    "form": ["accept-charset", "id", "method"],
+    "link": ["href", "type"],
+    "qbsearch-input": ["class"],
+    "modal-dialog": ["aria-labelledby", "aria-modal", "aria-describedby"],
+    "button": ["aria-label", "class"],
+    "h3": ["class", "id"],
+    "nav": ["aria-label"],
+    "img": ["class", "alt"],
+    "details": ["class"],
+    "a": [],
+}
+
+# Dictionary storing tags to replace with new tags
+tags_to_replace = {
+    ("textarea", "input"): "<i>",
+    ("div"): "<p>",
+    ("svg", "img", "picture", "form"): "<img>"
+}
+
+# List of tags to remove from the HTML content
+tags_to_remove = ["style", "span", "class", "circle",
+    "meta", "head", "details-dialog", "path",
+    "auto-check", "rect", "script", "ellipse"]
+
+
+# Extracting specific content from table tags and counting occurrences
+for table in soup("table"):
+    counter = Counter()
+    for thead in table('thead'):
+        thead.decompose()
+    
+    for tbody in table('tbody'):
+        for tr in tbody('tr'):
+            for td in tr('td'):  
+                if 'aria-describedby' in td.attrs:
+                    counter[td['aria-describedby']] += 1
+            tr.decompose()
+        tbody.string = str([(k,v) for k,v in counter.items()])
+
+
+# Function to keep specified attributes for specified tags
+def keep_attributes(tags_to_store):
+    for tag, attrs_to_keep in tags_to_store.items():
+        tags = soup.find_all(tag)
+        for tag in tags:
+            attrs = list(tag.attrs.keys())
+            for attr in attrs:
+                if attr not in attrs_to_keep:
+                    del tag[attr]
+
+
+# Function to replace specified tags with new ones
+def replace_tags(tags_to_replace, soup):
+    for tags, new_tag in tags_to_replace.items():
+        for tag in tags:
+            for match in soup.find_all(tag):
+                match.replace_with(BeautifulSoup(new_tag, 'html.parser'))
+
+
+# Function to remove specified tags from the HTML content
+def remove_tags(tags_to_remove, soup):
+    for tag in tags_to_remove:
+        for match in soup.find_all(tag):
+            match.decompose()
+
+
+keep_attributes(tags_to_store)
+replace_tags(tags_to_replace, soup)
+remove_tags(tags_to_remove, soup)
+
+# Replace all ending tags with "</>"
+modified_html_str = re.sub(r'</\w+>', '</>', str(soup))
+
+print(modified_html_str)
